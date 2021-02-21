ac9448e7013b0cc6ac69d1be77907e7dcbceb687
Karim <karim@gumgum.com>
2021-02-21 15:02:31 -0800

    WIP - Optimizing for HelloFresh US version + tweaking some formatting

--- src/scraper/providers/hellofresh.py
+++ src/scraper/providers/hellofresh.py
@@ -4,7 +4,7 @@ from base64 import b64encode
 import requests
 
 from scraper.providers import BaseScraper
-from scraper.providers.utils import format_measurements
+from scraper.providers.utils import format_measurements, format_nutrition
 
 
 class HelloFreshScraper(BaseScraper):
@@ -21,9 +21,8 @@ class HelloFreshScraper(BaseScraper):
             "source_url": self.url,
             "servings": "2 servings",
             "categories": ["HelloFresh"],
-            "prep_time": "10 minutes",
-            "cook_time": self.cook_time,
-            "total_time": self.cook_time,
+            "prep_time": self.prep_time,
+            "total_time": self.total_time,
             "photo": self.photo,
         }
 
@@ -37,20 +36,20 @@ class HelloFreshScraper(BaseScraper):
 
     @property
     def ingredients(self) -> str:
-        measurements = ["Stück", "ml", "g"]
+        measurements = ["ounce", "tablespoon", "unit", "cup", "teaspoon"]
         parent_tag = self.soup.find("div", {"data-test-id": "recipeDetailFragment.ingredients"})
         div = list(parent_tag)[3]
         in_the_box = [format_measurements(tag.text, measurements) for tag in list(div.children)[0]]
         div = list(parent_tag)[4]
-        in_your_pantry = [tag.text for tag in list(div.children)[1]]
+        in_your_pantry = [format_measurements(tag.text, measurements) for tag in list(div.children)[1].contents[0]]
         return "\n".join(in_the_box + in_your_pantry)
 
     @property
     def nutritional_info(self) -> str:
-        measurements = ["Portion", "(kJ)", "kcal)"]
+        measurements = ["Portion", "kJ", "kcal", "g", "mg"]
         parent_tag = self.soup.find("div", {"data-test-id": "recipeDetailFragment.nutrition-values"})
         div = list(parent_tag)[-1]
-        return "\n".join([format_measurements(tag.text, measurements) for tag in list(div.children)[0] if tag.text])
+        return "\n".join([format_nutrition(line) for line in list(div.children)[0]])
 
     @property
     def directions(self) -> str:
@@ -61,8 +60,12 @@ class HelloFreshScraper(BaseScraper):
         return "\n\n".join([x[:1] + ". " + x[1:] for x in final_directions])
 
     @property
-    def cook_time(self) -> str:
+    def total_time(self) -> str:
         return self.soup.find("span", {"data-translation-id": "recipe-detail.preparation-time"}).next.next.text
+
+    @property
+    def prep_time(self) -> str:
+        return self.soup.find("span", {"data-translation-id": "recipe-detail.cooking-time"}).next.next.text
 
     @property
     def photo(self) -> str:
--- src/scraper/providers/utils.py
+++ src/scraper/providers/utils.py
@@ -3,3 +3,9 @@ def format_measurements(text, measurements):
         if m in text:
             return text[: text.find(m) + len(m)] + " " + text[text.find(m) + len(m) :]
     return text
+
+def format_nutrition(line):
+    if (len(line) == 2) :
+        return line.contents[0].text + " " + line.contents[1].text
+    else:
+        return line.text
